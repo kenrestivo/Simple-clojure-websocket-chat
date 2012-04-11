9405bba8efeb19f6320966766f189da2aa278689
ken restivo <krestivo@restivo.org>
2012-04-10 19:57:31 -0700

    update readme, add examples

---
+++ README.md
@@ -0,0 +1,17 @@
+# Simple Clojure Websockets Chat
+
+Simple chatroom demo, written in Clojure, using webbit.
+
+I'm new to Clojuer; this was just an experiment. I used it as a back-end for another experiment.
+
+## Usage
+
+Just start the server using "lein run".
+
+Ther's some Javascript boilerplate for exercising the server is in test/webbittest.js, which you can execute in your browser's Javascript REPL (i.e. FIrebug or the Chrome console, etc).
+
+## License
+
+Copyright (C) 2012 ken restivo
+
+Distributed under the Eclipse Public License, the same as Clojure.
--- test/webbittest.js
+++ test/webbittest.js
@@ -1,8 +1,31 @@
-ws = new MozWebSocket('ws://localhost:8072/chatsocket');
+// manual setup
+if(typeof WebSocket === 'undefined'){
+    WebSocket = MozWebSocket;
+}
 
-ws.onopen = console.log;
-ws.onclose = console.log;
-ws.onerror = console.log;
-ws.onmessage = function(e){console.log(JSON.parse(e.data));};
+ws = new WebSocket('ws://localhost:8072/chatsocket');
 
 function send(outgoing) { ws.send(JSON.stringify(outgoing));}
+
+// replace these console.log handlers with something more useful
+ws.onopen = console.log;  
+ws.onclose = console.log; 
+ws.onerror = console.log; 
+ws.onmessage = function(e){console.log(JSON.parse(e.data));};
+
+
+////// examples
+
+// logging in
+send({action:'LOGIN', loginUsername: "My Name"});
+
+
+// exercising the userlist function
+send({action: "USERLIST"});
+
+// sending stuff
+send({action: "SAY", message: "This is a test message"});
+
+// clients can send any json-ish data not just messages
+send({action: "SPRAY", data: {foo: "bar", baz: 1, y:2}});
+
